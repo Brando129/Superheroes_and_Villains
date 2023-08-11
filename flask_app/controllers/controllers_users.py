8db285319d95b91cf25a90996b79d21c30c50d30
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-11 14:31:00 -0400

    Working on the  search heroes and villains route.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -1,12 +1,28 @@
 from flask_app import app
-from flask import render_template
+from flask import render_template, request, session, redirect
 import os
 import requests
 
 # API Key
 header = os.environ.get('KEY')
 
+# Get Routes
 # Route for rendering the Home Page.
 @app.get('/')
 def index():
     return render_template('homepage.html')
+
+# Route for rendering Show heroes and villains page.
+@app.get('/show/heroes/villains')
+def show_heroes_and_villains():
+    return render_template('show_heroes_and_villains.html')
+
+# Post Routes
+@app.post('/search/heroes/villains')
+def search_heroes_and_villains():
+    name = request.form['name']
+    url = f"https://superheroapi.com/api/{header}/search/{name}"
+    response = requests.get(url)
+
+    session['name'] = response.json()['name']
+    return redirect('/show/heroes/villains')
