70e0e5b6ef00ca556012269324edef16413a078e
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-11 18:39:44 -0400

    Mapping out the card on show heroes and villains.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -23,21 +23,14 @@ def show_heroes_and_villains():
 def search_heroes_and_villains():
     name = request.form['name']
     url = f"https://superheroapi.com/api/{header}/search/{name}"
-    # url = f"https://superheroapi.com/api/{header}/{id}"
     response = requests.get(url)
 
-    # session['id'] = response.json()['results'][0]['id']
-    # session['image'] = response.json()['results'][0]['image']
-    # pprint(session['id'])
-    # pprint(session['image'])
     id = response.json()['results'][0]['id']
     url_two = f"https://superheroapi.com/api/{header}/{id}"
     response_two = requests.get(url_two)
 
-    # session['image'] = f"https://superheroapi.com/api/{header}/{id}/image"
     session['image'] = response_two.json()['image']['url']
     session['name'] = response_two.json()['name']
     session['powerstats'] = response_two.json()['powerstats']
-    print(session['image'])
 
     return redirect('/show/heroes/villains')
