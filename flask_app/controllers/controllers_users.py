ee46e7c0f14d13d26ad5913583d904dc5f047c0e
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-11 16:39:56 -0400

    Working on the search heroes and villans route.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -2,6 +2,7 @@ from flask_app import app
 from flask import render_template, request, session, redirect
 import os
 import requests
+from pprint import pprint
 
 # API Key
 header = os.environ.get('KEY')
@@ -22,7 +23,21 @@ def show_heroes_and_villains():
 def search_heroes_and_villains():
     name = request.form['name']
     url = f"https://superheroapi.com/api/{header}/search/{name}"
+    # url = f"https://superheroapi.com/api/{header}/{id}"
     response = requests.get(url)
 
-    session['name'] = response.json()['name']
+    # session['id'] = response.json()['results'][0]['id']
+    # session['image'] = response.json()['results'][0]['image']
+    # pprint(session['id'])
+    # pprint(session['image'])
+    id = response.json()['results'][0]['id']
+    url_two = f"https://superheroapi.com/api/{header}/{id}"
+    response_two = requests.get(url_two)
+
+    # session['image'] = f"https://superheroapi.com/api/{header}/{id}/image"
+    session['image'] = response_two.json()['image']['url']
+    session['name'] = response_two.json()['name']
+    session['powerstats'] = response_two.json()['powerstats']
+    print(session['image'])
+
     return redirect('/show/heroes/villains')
