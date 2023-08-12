e962649c2d2d8492c03f333231e508dc3764a169
Brandon Colangelo <brandoncancode@gmail.com>
2023-08-12 11:19:26 -0400

    Added Bio/Connections/Powerstats to search route.

--- flask_app/controllers/controllers_users.py
+++ flask_app/controllers/controllers_users.py
@@ -29,6 +29,7 @@ def search_heroes_and_villains():
     url_two = f"https://superheroapi.com/api/{header}/{id}"
     response_two = requests.get(url_two)
 
+    # Appearance
     session['image'] = response_two.json()['image']['url']
     session['name'] = response_two.json()['name']
     session['gender'] = response_two.json()['appearance']['gender']
@@ -39,6 +40,32 @@ def search_heroes_and_villains():
     session['kilograms'] = response_two.json()['appearance']['weight'][1]
     session['eye_color'] = response_two.json()['appearance']['eye-color']
     session['hair_color'] = response_two.json()['appearance']['hair-color']
-    session['powerstats'] = response_two.json()['powerstats']
+    # End of Appearance
+
+    # Biography
+    session['full_name'] = response_two.json()['biography']['full-name']
+    session['alter_egos'] = response_two.json()['biography']['alter-egos']
+    session['aliases'] = response_two.json()['biography']['aliases'] # needs for loop
+    session['place_of_birth'] = response_two.json()['biography']['place-of-birth']
+    session['base'] = response_two.json()['work']['base']
+    session['occupation'] = response_two.json()['work']['occupation']
+    session['first_appearance'] = response_two.json()['biography']['first-appearance']
+    session['publisher'] = response_two.json()['biography']['publisher']
+    session['alignment'] = response_two.json()['biography']['alignment']
+    # End of Biography
+
+    # Connections
+    session['group_affiliation'] = response_two.json()['connections']['group-affiliation']
+    session['relatives'] = response_two.json()['connections']['relatives']
+    # End of Connections
+
+    # Powerstats
+    session['intelligence'] = response_two.json()['powerstats']['intelligence']
+    session['strength'] = response_two.json()['powerstats']['strength']
+    session['speedd'] = response_two.json()['powerstats']['speed']
+    session['durability'] = response_two.json()['powerstats']['durability']
+    session['power'] = response_two.json()['powerstats']['power']
+    session['combat'] = response_two.json()['powerstats']['combat']
+    # End of Powerstats
 
     return redirect('/show/heroes/villains')
